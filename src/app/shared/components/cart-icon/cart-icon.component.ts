6b63f59e355d9e44c47d1cd90302e27346349bb3
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2025-06-18 22:17:30 +0200

    update

---
+++ src/app/shared/components/cart-icon/cart-icon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './cart-icon.component.html',
+  styleUrl: './cart-icon.component.scss'
+})
+export class CartIconComponent {
+
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: [
     "./src/**/*.{html,ts}", "./src/**/*"
   ],
   theme: {
-    extend: {},
-  },
+    extend: {
+      fontFamily: {
+        // Ustaw Roboto jako pierwszą opcję dla czcionek sans-serif
+        sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+      },
+    },
+
   plugins: [],
+}
 }
 
