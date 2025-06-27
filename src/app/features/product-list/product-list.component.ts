9a82b562846733b2c92b5ea0399047170bc003d4
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2025-06-28 00:56:09 +0200

    update

--- src/app/features/product-list/product-list.component.ts
+++ src/app/features/product-list/product-list.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject, OnInit } from '@angular/core';
+import { ProductCardComponent } from "../../shared/components/product-card/product-card.component";
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
--- src/app/layout/main/main.component.ts
+++ src/app/layout/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ProductListComponent } from "../../features/product-list/product-list.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [ProductListComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
